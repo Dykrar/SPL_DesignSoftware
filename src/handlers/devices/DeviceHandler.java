a3df42b36f6ae6704e61e6505128255e7030db77
Zerlyan <gongas707@gmail.com>
2021-12-30 23:41:17 +0000

    comentado again

--- src/handlers/devices/DeviceHandler.java
+++ src/handlers/devices/DeviceHandler.java
@@ -2,8 +2,14 @@ package handlers.devices;
 
 import com.bezirk.middleware.addressing.ZirkEndPoint;
 
+import dispositivos.CampainhaInteligente;
 import dispositivos.bezirk.Dispositivo;
 import eventos.bezirk.DispositivoEvent;
+/**
+ * import com.google.inject.Inject;
+import com.google.inject.name.Named;
+ */
+
 /**
  * import zirk.devices.Device;
 import zirk.devices.MotionDetector;
@@ -11,12 +17,30 @@ import zirk.devices.SmartBulb;
 import zirk.devices.Wearable;
 import zirk.events.DeviceEvent;
  */
+import dispositivos.bezirk.*;
 
 
 public class DeviceHandler {
 
 	private static DeviceHandler instance;
+	/**
+	 * 	@Inject @Named("MovementDetector")
+    private static DetetorMovimento detetorMovimento;
+
+    @Inject @Named("SmartLights")
+    private static LampadaInteligente lampadaInteligente;
+
+    @Inject @Named("SmartButton")
+    private static BotaoInteligente botaoInteligente;
+
+    @Inject @Named("SmartDoorBell")
+    private static  CampainhaInteligente campainhaInteligente ;
+
+    @Inject @Named("SmartOpenCloseDoor")
+    private static DetetorAberturaPorta detetorAberturaPorta;
 	
+	 */
+
 	private DeviceHandler() {
 	}
 
