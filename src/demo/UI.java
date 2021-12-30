9666bb2538d062b3335f8b3a8aa19712b8d6d6b1
Zerlyan <gongas707@gmail.com>
2021-12-30 23:49:46 +0000

    coiso

--- src/demo/UI.java
+++ src/demo/UI.java
@@ -12,8 +12,8 @@ public class UI {
 		try {
 			Logger cmRootLogger = Logger.getLogger("default.config");
 			cmRootLogger.setLevel(java.util.logging.Level.OFF);
-			String conFile = System.getProperty("java.util.logging.config.file");
-			if (conFile == null) {
+			String configFile = System.getProperty("java.util.logging.config.file");
+			if (configFile == null) {
 			      System.setProperty("java.util.logging.config.file", "ignoreAllSphinx4LoggingOutput");
 			}
 			  
